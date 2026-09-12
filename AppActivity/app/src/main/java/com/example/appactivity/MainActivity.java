eb3766d8d0cce39d93f6f700910b3eeca977e1f5
Sana Bahrini <sanabahrini@gmail.com>
2026-09-12 15:27:36 -0400

    Ajout de l'exercice AppActivity

---
+++ AppActivity/app/src/main/java/com/example/appactivity/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.appactivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(MainActivity.this, "On pause", Toast.LENGTH_LONG).show();
+        Log.v("Sana","On Pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(MainActivity.this, "On stop", Toast.LENGTH_LONG).show();
+        Log.v("Sana","On Stop");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(MainActivity.this, "On restart", Toast.LENGTH_LONG).show();
+        Log.v("Sana","On Restart");
+
+    }
+}
