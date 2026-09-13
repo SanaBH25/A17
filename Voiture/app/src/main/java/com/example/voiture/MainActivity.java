8dea81b73233ca2a59821c2341fdd59bb21edf51
Sana Bahrini <sanabahrini@gmail.com>
2026-09-12 20:15:26 -0400

    Voiture : layout ListView plein écran

--- Voiture/app/src/main/java/com/example/voiture/MainActivity.java
+++ Voiture/app/src/main/java/com/example/voiture/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.voiture;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    //private ListView listing;
+    //private String[] data = {"Toyota", "Honda", "Ford", "BMW", "Mercedes", "Audi", "Tesla"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //listing = findViewById(R.id.listing);
+        //ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        //listing.setAdapter(adapter);
     }
 }
